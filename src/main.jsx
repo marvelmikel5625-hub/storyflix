21026677b3770ec2ed309638aae9e85628b9ff0b
Nosiru Micheal <marvelmikel5625@gmail.com>
2026-08-22 22:22:51 +0100

    Wrap App component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <h1>StoryFlix</h1>
-      <p>Movies, series, stories and novels.</p>
-    </div>
-  );
-}
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
