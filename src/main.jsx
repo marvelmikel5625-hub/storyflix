5285ae009fe8354bea258717a336820804bea7b3
Nosiru Micheal <marvelmikel5625@gmail.com>
2026-08-22 22:18:11 +0100

    Add main.jsx with basic React app structure

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>StoryFlix</h1>
+      <p>Movies, series, stories and novels.</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
