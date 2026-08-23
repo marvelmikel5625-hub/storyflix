b45dddf99e919cc8b19179d7e24ba6754d4f3ca1
Nosiru Micheal <marvelmikel5625@gmail.com>
2026-08-23 12:23:56 +0100

    Add Search route and improve 404 page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
+
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import Series from "./pages/Series";
 import Stories from "./pages/Stories";
+import Search from "./pages/Search";
 
 import MovieDetails from "./pages/MovieDetails";
 import SeriesDetails from "./pages/SeriesDetails";
@@ -12,13 +14,13 @@ import SeriesDetails from "./pages/SeriesDetails";
 function App() {
   return (
     <div className="min-h-screen bg-[#070707] text-white">
-
       <Header />
 
       <Routes>
-
+        {/* Home */}
         <Route path="/" element={<Home />} />
 
+        {/* Movies */}
         <Route path="/movies" element={<Movies />} />
 
         <Route
@@ -26,6 +28,7 @@ function App() {
           element={<MovieDetails />}
         />
 
+        {/* Series */}
         <Route path="/series" element={<Series />} />
 
         <Route
@@ -33,21 +36,44 @@ function App() {
           element={<SeriesDetails />}
         />
 
+        {/* Stories */}
         <Route path="/stories" element={<Stories />} />
 
+        {/* Search */}
+        <Route
+          path="/search"
+          element={<Search />}
+        />
+
+        {/* 404 */}
         <Route
           path="*"
           element={
-            <div className="flex min-h-screen items-center justify-center">
-              <h1 className="text-2xl font-bold">
-                Page not found
-              </h1>
-            </div>
+            <main className="flex min-h-[70vh] items-center justify-center px-5">
+              <div className="text-center">
+                <p className="text-sm font-bold uppercase tracking-[0.2em] text-red-500">
+                  404
+                </p>
+
+                <h1 className="mt-3 text-4xl font-black">
+                  Page Not Found
+                </h1>
+
+                <p className="mt-3 text-sm text-zinc-500">
+                  The page you're looking for doesn't exist.
+                </p>
+
+                <a
+                  href="/"
+                  className="mt-7 inline-flex rounded-lg bg-white px-6 py-3 text-sm font-bold text-black transition hover:bg-zinc-200"
+                >
+                  Back to Home
+                </a>
+              </div>
+            </main>
           }
         />
-
       </Routes>
-
     </div>
   );
 }
