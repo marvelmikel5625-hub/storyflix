e423ac4b610d649596be7302f2dbb30d488622de
Nosiru Micheal <marvelmikel5625@gmail.com>
2026-08-23 05:58:05 +0100

    Create content.js with movies, series, and stories
    Added movie, series, and story data to content.js.

---
+++ src/data/content.js
@@ -0,0 +1,226 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Dune: Part Two",
+    year: "2024",
+    rating: "8.6",
+    genre: "Sci-Fi",
+    image:
+      "https://image.tmdb.org/t/p/w780/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg",
+    description:
+      "Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.",
+  },
+  {
+    id: 2,
+    title: "Oppenheimer",
+    year: "2023",
+    rating: "8.6",
+    genre: "Drama",
+    image:
+      "https://image.tmdb.org/t/p/w780/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+    description:
+      "The story of J. Robert Oppenheimer and his role in the development of the atomic bomb.",
+  },
+  {
+    id: 3,
+    title: "The Batman",
+    year: "2022",
+    rating: "7.8",
+    genre: "Crime",
+    image:
+      "https://image.tmdb.org/t/p/w780/74xTEgt7R36Fpooo50r9T25onhq.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/74xTEgt7R36Fpooo50r9T25onhq.jpg",
+    description:
+      "Batman ventures into Gotham City's underworld when a sadistic killer leaves behind a trail of cryptic clues.",
+  },
+  {
+    id: 4,
+    title: "Interstellar",
+    year: "2014",
+    rating: "8.7",
+    genre: "Adventure",
+    image:
+      "https://image.tmdb.org/t/p/w780/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+    description:
+      "Explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+  },
+  {
+    id: 5,
+    title: "Spider-Man: No Way Home",
+    year: "2021",
+    rating: "8.0",
+    genre: "Action",
+    image:
+      "https://image.tmdb.org/t/p/w780/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+    description:
+      "Peter Parker's identity is revealed, bringing dangerous visitors from other realities into his world.",
+  },
+  {
+    id: 6,
+    title: "Avatar: The Way of Water",
+    year: "2022",
+    rating: "7.6",
+    genre: "Fantasy",
+    image:
+      "https://image.tmdb.org/t/p/w780/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
+    description:
+      "Jake Sully and Neytiri have formed a family and do everything to stay together.",
+  },
+  {
+    id: 7,
+    title: "Avengers: Endgame",
+    year: "2019",
+    rating: "8.3",
+    genre: "Action",
+    image:
+      "https://image.tmdb.org/t/p/w780/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
+    description:
+      "The Avengers assemble once more to reverse the devastation caused by Thanos.",
+  },
+  {
+    id: 8,
+    title: "John Wick",
+    year: "2014",
+    rating: "7.4",
+    genre: "Action",
+    image:
+      "https://image.tmdb.org/t/p/w780/fZPSd91yGE9fCcCe6OoQr6E3Bev.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/fZPSd91yGE9fCcCe6OoQr6E3Bev.jpg",
+    description:
+      "An ex-hitman returns to the criminal underworld after an attack on the only thing he had left.",
+  },
+];
+
+export const series = [
+  {
+    id: 101,
+    title: "Stranger Things",
+    year: "2016",
+    rating: "8.6",
+    genre: "Sci-Fi",
+    image:
+      "https://image.tmdb.org/t/p/w780/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg",
+  },
+  {
+    id: 102,
+    title: "The Last of Us",
+    year: "2023",
+    rating: "8.6",
+    genre: "Drama",
+    image:
+      "https://image.tmdb.org/t/p/w780/uKvVjHNqB5VmOrdxqAt2F7J78ED.jpg",
+  },
+  {
+    id: 103,
+    title: "Wednesday",
+    year: "2022",
+    rating: "8.0",
+    genre: "Mystery",
+    image:
+      "https://image.tmdb.org/t/p/w780/9PFonBhy4cQy7Jz20NpMygczOkv.jpg",
+  },
+  {
+    id: 104,
+    title: "House of the Dragon",
+    year: "2022",
+    rating: "8.4",
+    genre: "Fantasy",
+    image:
+      "https://image.tmdb.org/t/p/w780/7QMsOTMUswlwxJP0rTTZfmz2tX2.jpg",
+  },
+  {
+    id: 105,
+    title: "The Boys",
+    year: "2019",
+    rating: "8.7",
+    genre: "Action",
+    image:
+      "https://image.tmdb.org/t/p/w780/stTEycfG9928HYGEISBFaG1ngjM.jpg",
+  },
+  {
+    id: 106,
+    title: "Breaking Bad",
+    year: "2008",
+    rating: "9.5",
+    genre: "Crime",
+    image:
+      "https://image.tmdb.org/t/p/w780/ztkUQFLlC19CCMYHW9o1zWhJRNq.jpg",
+  },
+];
+
+export const stories = [
+  {
+    id: 201,
+    title: "The Alpha's Last Forbidden Bond",
+    author: "Mikel Marvel",
+    genre: "Werewolf Romance",
+    rating: "4.9",
+    views: "2.4M",
+    image:
+      "https://images.unsplash.com/photo-1511108690759-009324a90311?w=800&q=85",
+  },
+  {
+    id: 202,
+    title: "The Billionaire's Secret Bride",
+    author: "Lena Rose",
+    genre: "Billionaire Romance",
+    rating: "4.8",
+    views: "1.8M",
+    image:
+      "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?w=800&q=85",
+  },
+  {
+    id: 203,
+    title: "Moonlight Academy",
+    author: "Ava Knight",
+    genre: "Fantasy",
+    rating: "4.7",
+    views: "954K",
+    image:
+      "https://images.unsplash.com/photo-1532012197267-da84d127e765?w=800&q=85",
+  },
+  {
+    id: 204,
+    title: "The Vampire's Promise",
+    author: "Evelyn Black",
+    genre: "Vampire Romance",
+    rating: "4.9",
+    views: "1.2M",
+    image:
+      "https://images.unsplash.com/photo-1512820790803-83ca734da794?w=800&q=85",
+  },
+  {
+    id: 205,
+    title: "The King's Hidden Daughter",
+    author: "Mia Carter",
+    genre: "Royal Romance",
+    rating: "4.8",
+    views: "731K",
+    image:
+      "https://images.unsplash.com/photo-1544947950-fa07a98d237f?w=800&q=85",
+  },
+  {
+    id: 206,
+    title: "Married to My Enemy",
+    author: "Sophia James",
+    genre: "Enemies to Lovers",
+    rating: "4.9",
+    views: "1.5M",
+    image:
+      "https://images.unsplash.com/photo-1495446815901-a7297e633e8d?w=800&q=85",
+  },
+];
